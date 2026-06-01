c21668259252678d25d235752ce9b8180f475628
Hung <22645171.hung@student.iuh.edu.vn>
2026-06-01 11:33:29 +0700

    solution for bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import { createHash } from "crypto";
 
 export type Block = {
   index: number;
@@ -9,5 +10,16 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  if (
+    !block ||
+    typeof block.index !== "number" ||
+    !block.timestamp ||
+    !Array.isArray(block.transactions) ||
+    !block.previous_hash ||
+    !block.current_hash
+  ) {
+    return false;
+  }
+  const data = `${block.index}${block.timestamp}${JSON.stringify(block.transactions)}${block.previous_hash}`;
+  return block.current_hash === createHash("sha256").update(data).digest("hex");
 }
